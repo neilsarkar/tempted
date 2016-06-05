a967d03ac771984cc18891f16e03bfa85066c3b8
Neil Sarkar <neil.sarkar@gotinder.com>
2016-06-05 15:33:53 -0700

    Checkpoint: pull urges as results array from database

--- tempted/UrgesViewController.swift
+++ tempted/UrgesViewController.swift
@@ -12,17 +12,15 @@ import RealmSwift
 class UrgesViewController : UICollectionViewController {
     let reuseIdentifier = "UrgeCell"
     let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    var urges: Results<Urge>?
     
-    var searches = ["Cool", "Nice", "Great"]
-    var urge:Urge?
-    
-    func urgeForIndexPath(indexPath: NSIndexPath) -> String {
-        return searches[indexPath.row]
+    func urgeForIndexPath(indexPath: NSIndexPath) -> Urge {
+        return urges![indexPath.row]
     }
 
     override func viewDidLoad() {
         let realm = try! Realm()
-        urge = realm.objects(Urge).first
+        urges = realm.objects(Urge)
     }
     
     override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
@@ -30,15 +28,15 @@ class UrgesViewController : UICollectionViewController {
     }
     
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return urge == nil ? 0 : 1
+        return urges == nil ? 0 : urges!.count
     }
     
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! UrgeCell
-//        let urge = urgeForIndexPath(indexPath)
+        let urge = urgeForIndexPath(indexPath)
         
         cell.backgroundColor = UIColor.lightGrayColor()
-        cell.timeLabel.text = urge!.id
+        cell.timeLabel.text = urge.id
         return cell
     }
 }
