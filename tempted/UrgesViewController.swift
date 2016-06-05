1c6a66b261445e2a3bf187c38450bc2c12994c1b
Neil Sarkar <neil.sarkar@gotinder.com>
2016-06-05 14:39:42 -0700

    WIP: get collectionview showing items

---
+++ tempted/UrgesViewController.swift
@@ -0,0 +1,34 @@
+//
+//  UrgesViewController.swift
+//  tempted
+//
+//  Created by Neil Sarkar on 6/5/16.
+//  Copyright © 2016 Neil Sarkar. All rights reserved.
+//
+
+import UIKit
+
+class UrgesViewController : UICollectionViewController {
+    private let reuseIdentifier = "UrgeCell"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+    private var searches = ["Cool", "Nice", "Great"]
+    
+    func urgeForIndexPath(indexPath: NSIndexPath) -> String {
+        return searches[indexPath.item];
+    }
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return searches.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath)
+        cell.backgroundColor = UIColor.blackColor()
+        return cell
+    }
+}
