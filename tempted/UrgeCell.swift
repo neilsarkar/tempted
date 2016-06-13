7b569e43df637f70baca3c8b98e24aa271465220
Neil Sarkar <neil.sarkar@gotinder.com>
2016-06-12 19:10:59 -0700

    Add failure and loading states for map images

--- tempted/UrgeCell.swift
+++ tempted/UrgeCell.swift
@@ -22,6 +22,15 @@ class UrgeCell : UICollectionViewCell {
     @IBOutlet weak var mapImageView: UIImageView!
     @IBOutlet weak var loadingSpinner: UIActivityIndicatorView!
     @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var mapLoadFailedLabel: UILabel!
+    @IBOutlet weak var retryButton: UIButton!
+    
+    @IBAction func retryTapped(sender: UIButton) {
+        dispatch_async(dispatch_get_main_queue()) {
+            self.showLoading()
+        }
+        attemptLoadImage()
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -40,20 +49,52 @@ class UrgeCell : UICollectionViewCell {
             return
         }
 
-        stopLoading()
-        mapImageView.opaque = false
-        mapImageView.hnk_setImageFromURL(urge.mapImageUrl(Int(mapImageView.frame.width), height: Int(mapImageView.frame.height))!)
+
+        attemptLoadImage()
     }
+    
+    private func attemptLoadImage() {
+        if let mapUrl = urge.mapImageUrl(Int(mapImageView.frame.width), height: Int(mapImageView.frame.height)) {
+            mapImageView.hnk_setImageFromURL(mapUrl, failure: { error in
+                if( error?.code != -1009 ) {
+                    print("Unknown error", error)
+                }
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.showLoadFailed()
+                }
+
+            }, success: { image in
+                self.mapImageView.opaque = false
+                self.mapImageView.image = image
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.showLoaded()
+                }
+            })
+        } else {
+            print("Invalid map URL", urge.mapImageUrl(Int(mapImageView.frame.width), height: Int(mapImageView.frame.height)))
+        }
+    }
+    
+// MARK: interface state changes
 
     private func showLoading() {
         loadingSpinner.startAnimating()
         mapImageView.hidden = true
         deleteButton.hidden = true
+        retryButton.hidden = true
+        mapLoadFailedLabel.hidden = true
     }
     
-    private func stopLoading() {
+    private func showLoaded() {
         loadingSpinner.stopAnimating()
         mapImageView.hidden = false
         deleteButton.hidden = false
+    }
+    
+    private func showLoadFailed() {
+        // TODO: should this use a different cell? is there a cleaner way of hiding one whole and showing the other?
+        mapLoadFailedLabel.hidden = false
+        retryButton.hidden = false
+        loadingSpinner.stopAnimating()
     }
 }
